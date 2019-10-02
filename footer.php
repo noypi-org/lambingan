571ef3825bff11ee10173be08660a3e403193281
dev <dev@noypi.org>
2019-10-02 08:12:58 +0000

    d

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
   <footer class="footer">
     <div class="content has-text-centered">
-      <p>API/contents provided by <a href="#/?_=pinoy+tv" onclick="window.location.replace(atob('aHR0cHM6Ly9waW5veWJheS5vcmcvP189bGFtYmluZ2FuLmdh'));">Pinoy TV</a>. Passionately crafted by <a href="https://noypi.org">noypi</a>. UI/UX by <a href="https://bulma.io">bulma</a>. An <a href="https://github.com/">opensource</a> project of noypi.org</p>
+      <p>API/contents provided by <a href="#/?_=pinoy+tv" onclick="window.location.replace(atob('aHR0cHM6Ly9waW5veWJheS5vcmcvP189bGFtYmluZ2FuLmdh'));">Pinoy TV</a>. Passionately crafted by <a href="https://noypi.org">noypi</a>. UI/UX by <a href="https://bulma.io">bulma</a>. An <a href="https://github.com/noypi-org/lambingan">opensource</a> project of noypi.org</p>
     </div>
   </footer>
   <script src="/app.js"></script>
